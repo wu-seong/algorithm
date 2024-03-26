1cdcf9f2665fa8cd270f85d39ecfe4ac23c38608
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-03-26 13:16:54 +0900

    [Gold V] Title: 평범한 배낭, Time: 5176 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -7,17 +7,12 @@ for i in range(1, n + 1):
 
 Max = 0
 for i in range(1, n + 1):  
-    for j in range(1, k + 1):  
-
-        weight = item[i][0]
-        value = item[i][1]
-        
+    weight = item[i][0]
+    value = item[i][1]
+    for j in range(1, k + 1): 
         if j < weight:
             dp[i][j] = dp[i - 1][j]  
-            
-     
         else: 
             dp[i][j] = max(dp[i - 1][j],value + dp[i - 1][j - weight])
             
-
 print(dp[n][k])
