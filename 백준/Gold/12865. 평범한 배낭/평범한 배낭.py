a4030f736dc8a4c66ba933f7507bec75e7527303
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-08-13 01:37:17 +0900

    [Gold V] Title: 평범한 배낭, Time: 4664 ms, Memory: 225228 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,18 +1,24 @@
-n, k = map(int, input().split())  
-dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]  
+import sys
+input = sys.stdin.readline
 
-item = [[0, 0]]
-for i in range(1, n + 1):  
-    item.append(list(map(int, input().split())))
+N,K = map(int, input().rstrip().split())
+T = [ list(map(int, input().rstrip().split())) for _ in range(N) ]
 
-Max = 0
-for i in range(1, n + 1):  
-    weight = item[i][0]
-    value = item[i][1]
-    for j in range(1, k + 1): 
-        if j < weight:
-            dp[i][j] = dp[i - 1][j]  
-        else: 
-            dp[i][j] = max(dp[i - 1][j],value + dp[i - 1][j - weight])
-            
-print(dp[n][k])
+dp = [ [0 for _ in range(K+1)] for _ in range(N)]
+
+for i in range(1,K+1):
+    if i >= T[0][0]:
+        dp[0][i] = T[0][1]
+        
+for i in range(1,N):
+    w,v = T[i]
+    # 이전 부분해 가져오기 
+    for j in range(1,K+1):
+        # 새로 추가된 물건을 선택할 수 없을 때는 이전과 같음
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        # 새로 추가한 물건을 선택하고 나머지 무게를 이전 최댓값으로 채운 것이 더 크다면 최대값 갱신
+        else:
+            dp[i][j] = max(dp[i-1][j], v+dp[i-1][j-w])
+print(dp[N-1][K])
+        
