cb072e220f8633e04b6f11fd27d63b6d7e976a4e
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-08-14 23:37:37 +0900

    [Gold IV] Title: LCS 2, Time: 688 ms, Memory: 56548 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -8,18 +8,16 @@ s2.insert(0,0)
 M = len(s1)-1
 N = len(s2)-1
 
-dp = [ [[0,()] for _ in range(N+1)]for _ in range(M+1)]
+dp = [ [0 for _ in range(N+1)]for _ in range(M+1)]
 # [1]에 이전 문자열 위치를 저장 
 max_length = 0
 for i in range(1,M+1):
     for j in range(1,N+1):
         if s1[i] == s2[j]:
-            dp[i][j][0] = dp[i-1][j-1][0] + 1
-            dp[i][j][1] = (i-1,j-1)
+            dp[i][j] = dp[i-1][j-1] + 1
         else:
-            dp[i][j][0] = max(dp[i][j-1][0], dp[i-1][j][0])
-            dp[i][j][1] = (i,j-1) if dp[i][j-1][0] >= dp[i-1][j][0] else (i-1,j)
-        max_length = max(dp[i][j][0], max_length)
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+        max_length = max(dp[i][j], max_length)
 
 print("%d\n" %max_length)
 if max_length == 0:
@@ -29,11 +27,16 @@ if max_length == 0:
 n,m = N,M
 r_str = ""
 while n != 0 and m != 0:
-    next_n = dp[m][n][1]
-    # 새로운 문자가 추가된 경우에만 문자를 추가
-    if next_n == (m-1,n-1):
+    # 문자가 추가된 경우만 문자열에 추가 
+    if dp[m][n] > dp[m-1][n] and dp[m][n] > dp[m][n-1]:
         r_str = s2[n] + r_str
-    m,n = next_n
+        n = n-1
+        m = m-1
+        continue
+    # 이전과 같은 경우는 이전으로 되추적 
+    if dp[m-1][n] > dp[m][n-1]:
+        m = m-1
+    else:
+        n = n-1
 print("%s\n" %r_str)
-
     
