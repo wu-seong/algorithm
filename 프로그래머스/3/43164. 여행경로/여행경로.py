c633f4c57b9b2916ff102a7c2a6077079439e8d6
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-08-17 16:22:43 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
 import sys
 sys.setrecursionlimit(10**6)
 answer = []
-def dfs(graph, current, sources):
+def dfs(graph, current, footprint):
+    global answer
     # 티켓을 완전히 소진하는 것이 목적
     if not graph[current]:
         all_visited = True
@@ -10,55 +12,35 @@ def dfs(graph, current, sources):
                 all_visited = False
                 return
         if all_visited:
-            global answer
-            t = current
-            answer.append(t)
-            while not (t == "ICN" and not sources['ICN']):
-                t = sources[t].pop()
-                answer.append(t)
-            answer = answer[::-1]
+            footprint.append(current)
+            answer = footprint
         return
     for dst in reversed(graph[current]):
         graph[current].remove(dst)
-        sources[dst].append(current)
-        dfs(graph, dst, sources)
+        footprint.append(current)
+        dfs(graph, dst, footprint)
         if answer:
             return
         graph[current].append(dst)
-        sources[dst].pop()
-    # 티켓은 사용 시에 제거
-    # 호출로 돌아오면 티켓 복구
-    
-
-    # 방문 시 이전 목적지를 차례로 저장
+        footprint.pop()
     return
 def solution(tickets):
     # 그래프 정보 만들기
-    graph = {}
-    sources = {}
+    graph = defaultdict(list)
+    footprint = []
     for t in tickets:
         s,d = t
-        if s not in graph:
-            graph[s] = []
-        if s not in sources:
-            sources[s] = []
-        if d not in sources:
-            sources[d] = []
-        if d not in graph:
-            graph[d] = []
-        
         graph[s].append(d)
     for g in graph:
         graph[g].sort(reverse=True)
     # dfs로 완전 탐색 
     for f_dst in reversed(graph["ICN"]):
         graph["ICN"].remove(f_dst)
-        sources[f_dst].append("ICN")
-        dfs(graph, f_dst, sources)
+        footprint.append("ICN")
+        dfs(graph, f_dst, footprint)
         if answer:
             break
         graph["ICN"].append(f_dst)
-        sources[f_dst].pop()
-    # 저장한 이전 목적지로 역추적
+        footprint.pop()
     #print(answer)
     return answer
