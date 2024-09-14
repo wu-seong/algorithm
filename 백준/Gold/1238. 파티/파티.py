62855459f13750248cb61c88ee8cacea0858b20d
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-09-14 18:55:27 +0900

    [Gold III] Title: 파티, Time: 48 ms, Memory: 34372 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -2,7 +2,7 @@ import sys
 import heapq
 input = sys.stdin.readline
 
-def dijkstra(start):
+def dijkstra(start, g):
     q = []
 
     heapq.heappush(q, (0, start))
@@ -12,33 +12,36 @@ def dijkstra(start):
         dist, now = heapq.heappop(q)
         if dist > distance[now]:
             continue
-        for v,w in graph[now]:
+        for v,w in g[now]:
             cost = dist + w
             if cost < distance[v]:
                 distance[v] = cost
                 heapq.heappush(q, (cost, v))
 
-
 V,E,X = map(int, input().rstrip().split())
 
 graph = [ [] for _ in range(V+1)]
+r_graph = [ [] for _ in range(V+1)]
 for i in range(E):
     u,v,w = map(int, input().rstrip().split())
     graph[u].append((v,w))
+    r_graph[v].append((u,w))
     #print(graph)
     
 distance = [float('inf')] * (V+1)
-# A에서 다익스트라
 total_cost = [0]*(V+1)
-dijkstra(X)
+dijkstra(X, graph)
 for i in range(1,V+1):
     total_cost[i] += distance[i]
     #print(distance)
+distance = [float('inf')] * (V+1)
+dijkstra(X, r_graph)
 for i in range(1,V+1):
-    distance = [float('inf')] * (V+1)
-    dijkstra(i)
-    total_cost[i] += distance[X]
-    #print(distance) 
+    total_cost[i] += distance[i]
+    #print(distance)
+    #print(total_cost) 
 print(max(total_cost))
-# Z보다 큰 것들은 
-# A 에서 
+
+# 그래프를 역방향으로 바꾸면
+# 모든 지점 -> X로 가는 최소비용을 다익스트라 1번으로 구할 수 있다. (X를 시작지점으로 역방향 다익스트라)
+# 다익스트라의 대칭적인 특성(역방향으로 바뀌어도 경로만 반대로 바뀌고 출발지 - 목적지 비용은 같다.)
