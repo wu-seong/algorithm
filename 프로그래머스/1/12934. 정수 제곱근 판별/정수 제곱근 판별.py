b0acaddcf8b4acafdacb695c81078569062571d2
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-09-25 17:36:53 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -0,0 +1,10 @@
+def solution(n):
+    # n이 어떤 양수 정수의 제곱이라면 제곱근을 구했을 때, 정수일 것이다.
+    # 루트 n이 정수인지를 판단
+    # 정수 판단 방법 -> 1으로 나눈 몫이 원래와 같다면(소수 점이 존재한지 않는다면) 정수임
+    root = n**(1/2)
+    print(root)
+    if root // 1 == root:
+        return (root+1)**2
+    else:
+        return -1
