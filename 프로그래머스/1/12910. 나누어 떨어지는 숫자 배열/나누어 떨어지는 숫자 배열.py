281dfacb2e8d01074925347d58d5758223b029eb
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-09-25 21:38:01 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 3.51 ms, Memory: 13.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,12 @@
+def solution(arr, divisor):
+    # arr를 순회하며 divisor로 나누어 떨어지는지를 판단하여 리스트에 넣기
+    result = []
+    for num in arr:
+        if num % divisor == 0:
+            result.append(num)
+    # 리스트 오름차순 정렬
+    result.sort()
+    # 리스트가 비었다면 -1담아 리턴
+    if not result:
+        result.append(-1)
+    return result
