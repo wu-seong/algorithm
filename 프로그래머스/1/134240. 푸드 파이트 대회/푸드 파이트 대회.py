d370e4fc7d559c35354f794e9fb3beeb522b65c9
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-10-03 18:37:51 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,9 +1,6 @@
 def solution(food):
-    # 홀수이면 -1하고
     left = ""
     for i in range(1,len(food)):
-        if food[i] % 2 == 1:
-            food[i] -= 1
         left += str(i)*(food[i]//2)
     # 2로 나눈 몫 만큼 인덱스 문자열 추가            
     #print(left)
