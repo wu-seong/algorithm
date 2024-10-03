7efaa7a6be121b3c8a9da5fe46553b9ab9feeb84
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-10-03 22:38:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,15 +1,15 @@
 def solution(n, arr1, arr2):
-    # arr1,2 각각 이진 문자열로 바꾸기
-    # and연산하기
+    # arr1,2 각각 이진 문자열로 바꾸기 -> 비트 연산자 이용하면 안바꾸고도 or 연산 가능
+    # or연산하기 -> 각각을 or연산보다 '|' 비트 연산자 활용 가능
     # 1인 부분 # 0은 공백으로 출력하기
     result = []
     for num1, num2  in zip(arr1,arr2):
-        num1 = (bin(num1))[2:]
-        num2 = (bin(num2))[2:]
-        num1 = ('0'*(n-len(num1))) + num1
-        num2 = ('0'*(n-len(num2))) + num2
         #print(num1,num2)
-        temp = ['#' if int(num1[i]) or int(num2[i]) else ' ' for i in range(n)]
-        result.append("".join(temp))
+        or_bit_str = bin(num1|num2)[2:]
+        or_bit_str = ('0'*(n-len(or_bit_str))) + or_bit_str
+        or_bit_str = or_bit_str.replace('1', '#')
+        or_bit_str = or_bit_str.replace('0', ' ')
+        #print(or_bit_str)
+        result.append(or_bit_str)
     #print(result)
     return result
