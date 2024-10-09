c4e5cb693c04f92b9fa913871e8cef5d84402ff9
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-10-09 17:57:40 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4977.21 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,20 @@
+
+def solution(elements):
+    # 길이가 1~len(elements)까지인거 모두 구해서 카운팅하기
+    # 결과값을 저장해서 이미 존재하면 카운팅에서 제외
+    n = len(elements)
+    elements.extend(elements)
+    #print(elements)
+    
+    sums = set([])
+    cnt = 0
+    for length in range(1, n+1): # 길이
+        for start in range(n): # 시작점
+            temp = sum(elements[start:start+length])
+            if not temp in sums:
+                cnt += 1
+                sums.add(temp)
+    return cnt
+            
+        
+    
