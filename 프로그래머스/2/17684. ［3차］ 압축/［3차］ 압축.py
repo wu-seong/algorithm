d1c9a9fdb545fbf0ef0f46edd63ea588a377895d
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-10-20 16:25:29 +0900

    [level 2] Title: [3차] 압축, Time: 19.34 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,47 +1,41 @@
+def search_str(z_dict, target_str): # 문자열 색인 번호 반환, 없으면 0반환
+    for i in range(len(z_dict)):
+        if z_dict[i] == target_str:
+            return i
+    return 0
 def solution(msg):
     # 각 글자를 시작으로 가장 긴 문자열에 해당하는 색인번호를 대치
-    # 1000 * 1000 = 1,000,000
-    # 영문 사전 초기화
+    # 각 입력을 가장 긴 문자열로 압축
+    # 해당 길이의 문자열로 압축이 안되면 바로 이전의 압축을 채택
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    
-    z_dict = [""]*27 # 리스트 저장
+    z_dict = [''] * (27)
     for i in range(26):
         z_dict[i+1] = alpha[i]
-    #print(z_dict)
-    # 각 글자가 사전에 있는지를 확인
-    # 사전에 없을 때 까지 한글자씩 늘리기
-    # 없을 때를 찾으면 없는 것을 사전에 추가하고(w+c)
-    # 없을 때의 직전 색인번호를 결과에 추가(w)
-    result = []
+
+    result = [] # 답
     n = len(msg)
     start = 0
-    idx = 26
-    while start < n: # 모든 문자를 다 치환할 때 까지
-        # 사전을 순회하면서 만약 있다면 그 색인번호를 저장하고 break
-        # 그 다음 길이를 탐색, 없다면 찾는 문자열을 사전에 추가
+    while start < n:
         end = start + 1
-        searched_idx = 0 #이전에 찾은 문자
-        while True: # 다 탐색하거나, 없을 때 까지 반복            
-            search_str = msg[start:end] # 문자열 갱신
-            fail = True
-            #print("search_str", search_str)
-            for i in range(len(z_dict)): # 찾기
-                if search_str == z_dict[i] :
-                    searched_idx = i
-                    fail = False
-                    break
-            end += 1
-            if fail : # 찾는 것에 실패할 때
-                z_dict.append(search_str)
+        # 찾을 문자열
+        # 최대 end len일 때 까지 
+        searched_idx = 0
+        while end <= n:
+            target_str = msg[start:end]
+            # 해당 문자열이 사전에 있는지를 반환
+            res = search_str(z_dict, target_str)
+            if res: # 있다면 색인번호 기억하기
+                searched_idx = res
+                end += 1
+                if end > n:
+                    result.append(searched_idx)
+                    return result
+            else: # 없으면 기억한 색인번호 추가 및 찾는 문자열 사전에 추가
                 result.append(searched_idx)
-                start = end - 2
-                #print("next", result, start)
+                z_dict.append(target_str)
+                start = end - 1
                 break
-            elif end > n: # 나머지 문자열이 사전안에 다 있을 때
-                result.append(searched_idx)
-                start = end
-                break
-    #print(result)
+    print(result)
     return result
         
             
