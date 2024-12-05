b9f111ceb91eb5f8bd22f13fe49ef731a1ded86f
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2024-12-05 15:53:11 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 1, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/299307. 대장균의 크기에 따라 분류하기 1/대장균의 크기에 따라 분류하기 1.sql
@@ -0,0 +1,8 @@
+select id, case
+when SIZE_OF_COLONY <= 100 then 'LOW'
+when SIZE_OF_COLONY <= 1000 then 'MEDIUM'
+when SIZE_OF_COLONY > 1000 then 'HIGH'
+end as SIZE
+from ECOLI_DATA
+order by id
+
