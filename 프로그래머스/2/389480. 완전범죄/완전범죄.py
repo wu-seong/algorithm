fb6a8a91d18de457164cd1625d0cf40eafd53e27
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-03-04 19:30:57 +0900

    [level 2] Title: 완전범죄, Time: 4.96 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,54 +1,29 @@
-# def solution(info, n, m):
-#     length = len(info)
-#     dp = [[float('inf') for _ in range(m)] for _ in range(length)]
-#     # A를 선택
-#     a,b = info[0]
-#     dp[0][0] = a
-#     # B를 선택
-#     if b < m:
-#         dp[0][b] = 0
-#     print('초기화:', dp)
-    
-#     for i in range(1, len(info)):
-#         a,b = info[i]
-#         print(a,b)
-#         if m == 1:
-#             dp[i][0] = dp[i-1][0] + a
-#         for j in range(m-1, -1, -1):
-#             # A 선택하는 경우
-#             dp[i][j] = dp[i-1][j] + a
-#             # B 선택하는 경우 (j+b<m)
-#             if j + b < m:
-#                 dp[i][j + b] = min(dp[i][j + b], dp[i-1][j])
-#     for i in range(len(info)):
-#         print(dp[i])
-            
 def solution(info, n, m):
     length = len(info)
-    dp = [[float('inf')] * m for _ in range(length)]
-
-    # 첫 번째 물건 처리
-    a, b = info[0]
-    dp[0][0] = a  # A가 선택한 경우
+    dp = [[float('inf') for _ in range(m)] for _ in range(length)]
+    # A를 선택
+    a,b = info[0]
+    dp[0][0] = a
+    # B를 선택
     if b < m:
-        dp[0][b] = 0  # B가 선택한 경우
-
-    # DP 테이블 채우기
-    for i in range(1, length):
-        a, b = info[i]
-        
-        for j in range(m-1, -1, -1):  # 역순 탐색 (값이 덮어써지는 문제 방지)
-            # A를 선택하는 경우
-            dp[i][j] = min(dp[i][j], dp[i-1][j] + a)
-
-            # B를 선택하는 경우 (j+b가 m을 넘지 않는다면)
+        dp[0][b] = 0
+    print('초기화:', dp)
+    
+    for i in range(1, len(info)):
+        a,b = info[i]
+        print(a,b)
+        for j in range(m-1, -1, -1):
+            # A 선택하는 경우
+            dp[i][j] = dp[i-1][j] + a
+            # B 선택하는 경우 (j+b<m)
             if j + b < m:
                 dp[i][j + b] = min(dp[i][j + b], dp[i-1][j])
-
-    # 결과 찾기
-    min_value = float('inf')
-    for value in dp[length - 1]:
-        if value < n:
-            min_value = min(min_value, value)
-
-    return min_value if min_value != float('inf') else -1
+    for i in range(len(info)):
+        print(dp[i])
+        
+    result = min(dp[length-1])
+    print(result)
+    if result < n: 
+        return result
+    return -1
+   
