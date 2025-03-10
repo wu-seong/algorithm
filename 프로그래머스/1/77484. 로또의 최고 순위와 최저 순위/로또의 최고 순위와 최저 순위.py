9d0f8944733aa26953201683972e882c925f5757
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-03-10 10:00:48 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,22 +1,31 @@
+'''
+0을 제외한 숫자가 번호에 있는지 확인하고 카운팅
+순위: 7 - 맞춘 개수, 맞춘 개수 1이하이면 -> 6
+
+최고순위는 0의 개수를 맞춘 개수에 더하기
+'''
 def solution(lottos, win_nums):
-    # 원래 몇 개 맞았는지 비교
+    # 순회하면서 당첨 숫자 여부 확인
     cnt = 0
-    cnt_zero = 0
-    for i in range(6):
-        if lottos[i] == 0: #훼손된 것 카운팅
-            cnt_zero += 1
-            continue
-        if lottos[i] in win_nums: # 맞은 것 카운팅
+    zero = 0
+    for l in lottos:
+        if l == 0:
+            zero += 1
+        if l in win_nums:
             cnt += 1
-    # 최저는 + 0 개 최고는 0의 개수만큼
-    high = cnt + cnt_zero
-    low = cnt
-    # 최고, 최저 순위 계산 하여 저장
-    h_rank = 7-high
-    l_rank = 7-low
-    if h_rank == 7: # 0개도 6위
-        h_rank = 6
-    if l_rank == 7:
-        l_rank = 6
-    answer = [h_rank, l_rank]
-    return answer
+    print(cnt, zero)
+    # 0 카운팅
+    # 최악은 그냥 당첨 숫자로만 랭크 확인
+    if cnt <= 1:
+        bad = 6
+    else:
+        bad = 7 - cnt
+    
+    cnt += zero
+    if cnt <= 1:
+        good = 6
+    else:
+        good = 7 - cnt
+    return [good, bad]
+        
+   
