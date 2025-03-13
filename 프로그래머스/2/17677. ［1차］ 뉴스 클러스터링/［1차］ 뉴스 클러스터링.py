1e9af1473d0d07b13c70a2316b20daaa1358246e
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-03-13 18:12:26 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.60 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,45 +1,64 @@
-from collections import defaultdict
+'''
+다중 집합에서 교집합, 합집합 구하기
+딕셔너리를 이용해서 key: 개수 저장하기
+
+교집합: key가 겹치고, 둘 중 최솟값
+합집합: key가 겹치지 않으면 해당 값, 겹친다면 둘 중 최댓값
+'''
 def solution(str1, str2):
-    # 두 글자씩 끊어 원소로 만들기
-    # 영문자 아니면 버리기 (isAlpha)
-    # 모두 소문자로 만들기 (lower)
-    # 튜플에 저장 및 카운팅
-    #print(str1, str2)
-    key1 = []
+    str1, str2 = str1.lower(), str2.lower()
+    m_set1, m_set2 = dict(), dict()
+    # 다중집합 만들기
     for i in range(len(str1)-1):
-        p_str = str1[i:i+2]
-        if p_str.isalpha():
-            key1.append(p_str.lower())
-    key2 = []
+        s = str1[i:i+2]
+        if not s.isalpha():
+            continue
+        if not s in m_set1:
+            m_set1[s] = 1
+        else:
+            m_set1[s] += 1
     for i in range(len(str2)-1):
-        p_str = str2[i:i+2]
-        if p_str.isalpha():
-            key2.append(p_str.lower())
-    if not key1 and not key2:
-        return 65536
-    #print(key1, key2)
-    A = defaultdict(int)
-    B = defaultdict(int)
-    for key in key1:
-        A[key] += 1
-    for key in key2:
-        B[key] += 1
-    #print(A,B)
-    # 교집합 구하기
-    # B에 A와 같은 키가 있으면 min(A[key], B[key]) 하여 카운팅 합산이 교집합
-    du_cnt = 0
+        s = str2[i:i+2]
+        if not s.isalpha():
+            continue
+        if not s in m_set2:
+            m_set2[s] = 1
+        else:
+            m_set2[s] += 1
+    #print(m_set1, m_set2)
+    def intersection(a,b):
+        temp = dict()
+        for k1 in a:
+            if k1 in b:
+                temp[k1] = min(a[k1], b[k1])
+        return temp
+    def union(a,b):
+        temp = dict()
+        for k1 in a:
+            if k1 in b:
+                temp[k1] = max(a[k1], b[k1])
+            else:
+                temp[k1] = a[k1]
+        for k2 in b:
+            if not k2 in a:
+                temp[k2] = b[k2]
+        return temp
     
-    for key in A:
-        if key in B:
-            du_cnt += min(A[key],B[key])
-    # 합집합 구하기
-    # 둘다 카운팅 - 교집합
-    sum_cnt = 0
-    for key in A:
-        sum_cnt += A[key]
-    for key in B:
-        sum_cnt += B[key]
-    sum_cnt -= du_cnt
-    #print(du_cnt/sum_cnt)
-    return int(du_cnt/sum_cnt*65536)
+                
+    i_set = intersection(m_set1, m_set2)
+    u_set = union(m_set1, m_set2)
+    #print(i_set, u_set)
+    i_sum, u_sum = 0, 0
+    for key in i_set:
+        i_sum += i_set[key]
+    for key in u_set:
+        u_sum += u_set[key]
+    #print(i_sum, u_sum)
+    if u_sum:
+        print(i_sum/u_sum)
+        return int(i_sum/u_sum*65536)
+    else:
+        return 65536
+                
+        
         
