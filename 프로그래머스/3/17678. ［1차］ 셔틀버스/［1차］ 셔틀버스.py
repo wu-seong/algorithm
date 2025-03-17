72881ed87ebdbaed10314492ddb931c6c3ae590d
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-03-17 22:14:59 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 2.15 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -1,58 +1,67 @@
 '''
-가장 막차를 타야함
+크루원들의 시간 모두 분으로 통일하기
+버스가 도착하는 시간도 통일하기
+to min, to str 함수 둘 다 필요
 
-16
-1시간마다 45명을 태움
-
-가장 늦은 막차에 대기하는 크루가 n명 이상이면 해당 차를 못탐
-탈 수 있는 차 중에서 가장 마지막 차를 타야함
-시뮬레이션을 돌리면서 탈 수 있는 차인지를 판별하기
-
-timetable을 시간순으로 정렬한다.
-
-버스 시간 마다 timetable에서 가장 먼저온 사람을 pop한다.
-pop의 횟수가 n이면 그 차는 탈 수 없는 차이다. -> 마지막 타는 녀석보다 1분 빠르게 타기
-pop의 횟수가 n미만이면 그 차는 아직 탈 수 있는 차이다. -> 버스 출발할 때 시간에 타기
+콘이 탈 수 있는 시간 중 가장 늦은 시간을 구하기
+-> 현재 버스가 마지막일 때는 무조건 타야함. 마지막으로 탑승한 사람보다 1분 빠르게 도착하기
+-> 버스 도착 시간마다 먼저 대기하고 있는 사람 태우기, 그다음 1시간 동안 최대한 태우고 못 타는 사람은 대기열에 두기
+-> 마지막 버스의 마지막 승객이 되야 하는데, 마지막 버스의 마지막 승객이 도착한 사람의 시간을 구하고 1분 더 빠르게 도착하기
 '''
 
 from collections import deque
 def solution(n, t, m, timetable):
-    def translate(time):
-        hour, minute = time.split(":")
-        return int(hour)*60 + int(minute)
-    timetable = deque(sorted((map(translate, timetable))))
-    bus_times = []
-    time = 540
-    for i in range(n): 
-        bus_times.append(time)
-        time += t
-    result = 0
-    #print(bus_time)
+    def to_min(time):
+        hour, min_num = map(int, time.split(':'))
+        #print(hour*60 + min_num,'분')
+        return hour*60 + min_num
+    def to_str(min_num):
+        hour = str(min_num//60)
+        min_str = str(min_num % 60)
+        hour = hour.rjust(2,'0')
+        min_str = min_str.rjust(2,'0')
+        #print(hour+':' + min_str)
+        return hour+ ':' + min_str
+    # 타임테이블 변환
+    timetable = [to_min(s) for s in timetable]
+    timetable.sort(reverse=True)
     #print(timetable)
-    for bus_time in bus_times:
-        cnt = 0
-        # m-1명이 탔을 때 다음녀석도 타려고 한다면 m-1명과 같은 초에 타기
-        # 그렇지 않다면 버스가 출발할 때 시간으로 타기
-        while timetable and timetable[0] <= bus_time and cnt < m:
+    #to_str(to_min(timetable[0]))
+    wating = deque()
+    # 버스 시간 만들기
+    bus = [ 540 + t*i for i in range(n)]
+    #print(bus)
+    # 버스 태우기 시뮬
+    last = 0
+    result = 0
+    for i, b in enumerate(bus):
+        # 대기열에 있는 얘들 태우기, 최대 인원까지만
+        cur = 0
+        while wating and cur < m:
+            last = wating.popleft()
+            cur += 1
+        # timetable얘들 태우기
+        while cur < m and timetable and timetable[-1] <= b:
             #print(timetable)
-            c_time = timetable.popleft()
-            cnt += 1
-            if cnt == m:
-                result = max(result, c_time-1)
+            last = timetable.pop()
+            cur += 1
+        # 남는 얘들 대기열에 넣기
+        while timetable and timetable[-1] <= b:
+            wating.append(timetable.pop())
+        # 막차일 때
+        if i == n-1: 
+            # 다 찼으면 마지막 탄얘보다 앞
+            if cur == m:
+                result = last-1
+            # 안찼으면 막차 출발 시간
+            else:
+                result = b
+    #print(to_str(result))
+    return(to_str(result))
+                
+                
             
-        if cnt < m: 
-            result = max(result, bus_time)
-    hour = result//60
-    minute = result % 60
-    if hour < 10:
-        hour = '0' + str(hour)
-    else:
-        hour = str(hour)
-    if minute < 10:
-        minute = '0' + str(minute)
-    else:
-        minute = str(minute)
-    print(hour, ":", minute, sep='')
-    return hour + ":" + minute
-            
+        
+        
+        
     
