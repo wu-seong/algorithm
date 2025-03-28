3caa9c26459aedf964592e273eff8a345a0ffdc1
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-03-28 19:40:35 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,10 +1,6 @@
-with out_animal as(
-    select animal_id 
-    from animal_outs
-)
+
 select name, datetime
 from ANIMAL_INS
-where animal_id not in (select animal_id from out_animal)
+where animal_id not in (select animal_id from animal_outs)
 order by datetime
 limit 3
-
