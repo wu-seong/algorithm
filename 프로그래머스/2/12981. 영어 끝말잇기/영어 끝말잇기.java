eb99971bf065b129c61e808919332f488141b6aa
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-06-24 21:01:37 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,38 +1,10 @@
-import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        /*
-        끝말잇기 규칙
-        탈락한 사람의 번호/회차
-        
-        1. 이전 단어의 마지막 단어와 일치여부 확인
-        2. 중복 여부 확인
-        
-        words를 순회하면서 순서와 회차 카운팅
-        
-        끝까지 지켰으면 [0,0]
-        */
-        Set<String> used = new HashSet<>();
-        
-        int order = 0;
-        int round = 0;
-        char last = '\0';
-        for(String word: words){
-            order %= n;
-            // 순서가 돌아옴
-            if(order == 0){
-                round += 1;
-            }
-            //System.out.println(order +" " + round);
-            char first = word.charAt(0);
-            // 규칙 위반
-            if(!(last == '\0' || last == first) || used.contains(word)){
-                return new int[]{order+1, round};
-            }
-            last = word.charAt(word.length()-1);
-            order++;
-            used.add(word);
-        }
-        return new int[]{0,0};
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
+        System.out.println("Hello Java");
+
+        return answer;
     }
 }
