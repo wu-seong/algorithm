1fb85e8dd9d07563347ab7138a3fadb2f7bd3aed
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-07-01 08:05:37 +0900

    [level 1] Title: 추억 점수, Time: 1.69 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,31 +1,6 @@
-import java.util.*;
-
 class Solution {
-    public static void log(Object o){
-        System.out.println(o);
-    }
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        /*
-        각 사람의 점수를 map에 담기
-        점수가 없는 경우에는 0
-        */
-        Map<String, Integer> score = new HashMap<>();
-        int n = name.length;
-        for(int i=0; i<n; i++){
-            score.put(name[i], yearning[i]);
-        }
-        // for(String key: score.keySet()){
-        //     log(key + " " + score.get(key));
-        // }
-        int[] result = new int[photo.length];
-        for(int i=0; i<photo.length; i++){
-            String[] people = photo[i];
-            int sum = 0;
-            for(String p: people){
-                sum += score.getOrDefault(p, 0);
-            }
-            result[i] = sum;
-        }
-        return result;
+        int[] answer = {};
+        return answer;
     }
 }
