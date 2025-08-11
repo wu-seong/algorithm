724e066a96247d39b1dece1ce1e9b5c4d33fd7d6
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-08-12 08:04:20 +0900

    [level 3] Title: 야근 지수, Time: 490.05 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,6 +1,28 @@
-from heapq import heapify, heappush, heappop
+from heapq import heappop, heappush, heapify
 def solution(n, works):
-    heapify(works := [-i for i in works])
-    for i in range(min(n, abs(sum(works)))):
-        heappush(works, heappop(works)+1)
-    return sum([i*i for i in works])
+    '''
+    제곱은 큰 숫자에서 값이 클수록 늘어난다.
+    가장 작업량이 높은 work부터 줄인다.
+    works를 최대힙으로 만든다.
+    pop하고 1씩 줄인 것을 push한다
+    위를 n번 반복한다.
+    '''
+    # 최대 heap만들기
+    for i in range(len(works)):
+        works[i] = -works[i]
+    heapify(works)
+    #print(works)
+    # 큰 것 부터 일 하기
+    i = n
+    while works and i > 0:
+        value = -heappop(works)
+        if value - 1 != 0:
+            heappush(works, -(value - 1))
+        i -= 1
+    print(works)
+    # 피로도 구하기
+    result = 0
+    for p in works:
+        result += (p ** 2)
+    return result
+    
