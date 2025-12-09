0bda50cd2f041fe4fbad139df44c5224d1c3d8c2
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-12-09 23:58:00 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.12 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,18 +1,27 @@
+def to_upper(ch):
+    return chr(ord(ch) - 32)
+def to_lower(ch):
+    return chr(ord(ch) + 32)
+def is_upper(ch):
+    return ord('A')<= ord(ch) <=ord('Z')
+def is_lower(ch):
+    return ord('a')<= ord(ch) <=ord('z')
 def solution(s):
-    # 모든 알파벳을 소문자로 바꾸기
-    s = s.lower()
-    # 각 단어 공백 기준으로 구분하기
-    new_s = []
-    for word in list(s.split(" ")):
-        if word == "": # 연속해서 나오면 그대로
-            new_s.extend(" ")
-            continue
-        word_list = list(word)
-        if word[0].isalpha():
-            word_list[0] = word[0].upper()
-        word_list.append(" ")
-        new_s.extend(word_list)
-    new_s = new_s[:-1]
-    #print("".join(new_s))
-    return "".join(new_s)
-    # 각 단어의 첫 글자가 알파벳이면 대문자로 바꾸기 
+    # 이전 저장했던 문자가 공백이고 첫 글자가 소문자이면 -> 대문자
+    # 이전 저장했던 글자가 공백이 아니고 첫 글자가 대문자이면 -> 소문자
+    # 마지막 글자가 단어의 시작일 수도 있음
+    # 돌고 나서 마지막 한번 더 체크
+    result = to_upper(s[0]) if is_lower(s[0]) else s[0]
+    last = s[0]
+    for ch in s[1:]:
+        if last == ' ' and is_lower(ch):
+            result += to_upper(ch)
+        elif last != ' ' and is_upper(ch):
+            result += to_lower(ch)
+        else:
+            result += ch
+        last = ch
+    print(result)
+    return result
+            
+    
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=python3) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 0.27 ms
+메모리: 9.24 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 05일 18:24:34
+2025년 12월 09일 23:57:57
 
 ### 문제 설명
 
