34f57cc8839b242cbfebf01b3eb30e2c5fdadea6
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-12-11 08:24:03 +0900

    [level 2] Title: 올바른 괄호, Time: 6.92 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=python3) 
 
 ### 성능 요약
 
-메모리: 54.9 MB, 시간: 19.45 ms
+메모리: 9.43 MB, 시간: 6.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 12일 23:53:52
+2025년 12월 11일 08:23:59
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,25 @@
+'''
+(와 )는 짝궁이어야함
+-> 닫는 괄호가 왔을 때, 여는 괄호가 없으면 오류, 다 지났는데 괄호가 안닫혀도 오류
+
+( 가 오면 스택에 push )가 오면 스택에서 pop
+)가 왔는데 스택이 empty이면 오류
+다 순회했는데 스택이 empty가 아니면 오류
+'''
 def solution(s):
     stack = []
-    for c in s:
-        if c == '(':
-            stack.append(c)
-        else:
-            if not stack:
+    #print(stack)
+    for v in s:
+        if v == ')':
+            if stack:
+                stack.pop()
+            else:
                 return False
-            stack.pop()
-    if stack: 
+        else:
+            stack.append(0)
+    if stack:
         return False
     return True
+                
+            
+            
