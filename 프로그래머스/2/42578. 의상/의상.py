ed4521f770829ca3530e2d50729469de27110dff
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-12-13 10:24:23 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,30 +1,34 @@
 from collections import defaultdict 
-def solution(clothes):
-    # 최소 1개는 선택해야한다.
-    # 종류당 1개씩만 선택할 수 있다.
-    # 의상 1개 - 모든 의상의 수
-    # 의상 2개 
-    # 서로 다른 n(2<=n<=4)개를 뽑는데 같은 종류이면 카운팅하지 않는다
-    # 각 의상마다 어떤 종류인지를 바로 알 수 있도록 딕셔너리에 저장
-    # 30c1 + 30c2 + 30c3 + 30c4 충분(인줄 알았는데 아님, 종류가 30개일 수도 있다.) 
-    # -> 다른 방법 필요
-    
-    # 각 종류 마다 key, value를 set으로 저장하기
-    # key에서 종류를 1~n개를 뽑아 각 종류의 수만큼 곱한다.
-    # 근데 이미 key를 뽑는 것에서 30c15인데 ?
-    
-    # 그냥 각 종류 마다 일단 개수를 센다.
-    # 각 종류마다 1개를 선택하거나 선택하지 않는 경우를 곱연산 한다.
-    c_dict = defaultdict(int)
-    for cloth, c_type in clothes:
-        c_dict[c_type] += 1
+'''
+모두 합하여 다른 조합 ->
+
+종류별 1가지 의상만 가능
+
+최소 1개의 의상은 입는다.
+
+서로 다른 옷의 조합의 수 =
+각 종류 마다 의상의 수 + 1(안 입은 경우)를 서로 곱한다.
+아예안 입은 경우(1)은 뺸다.
+
+1. 전처리
+- clothes배열의 각 원소를 받아서, key에 카운팅한다.
+
+2. 계산
+'''
+def solution(clothes): 
+    clothes_cnt = defaultdict(int)
+    for c in clothes:
+        _, key = c
+        clothes_cnt[key] += 1
+    #print(clothes_cnt)
     result = 1
-    for key in c_dict:
-        result *= c_dict[key] + 1
-    print(result)
-    return result-1
-    
-    
-# 선택하지 않는 것을 선택한다고 생각하면 곱연산이 가능하다.
-# 1~n까지의 모든 조합을 구하는 것은 각 요소에 대해 선택/선택하지 않는 모든 경우를 구하고 모두 선택하지 않는 경우를 빼는 것과 같다.
-# combinations는 크기보다 큰 조합을 뽑는 경우 빈 리스트를 반환한다.
+    for key in clothes_cnt:
+        #print(key)
+        result *= (clothes_cnt[key] + 1)
+    return result - 1
+
+'''
+tc1)
+의상 수가 1개일 때 -> 1
+
+'''
