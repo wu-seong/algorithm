ce25ab6d8f0096f8fe40e6f2646fabf17c72229c
chang wu seong <95726704+wu-seong@users.noreply.github.com>
2025-12-18 23:47:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.36 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,25 +1,35 @@
 '''
-bfs해서 n,m에 도착하면 해당 level을 반환 만약 도착하지 못하고 queue가 종료되면 -1반환
+최단거리 문제
+bfs를 통해 해당 좌표에 도달 시 depth를 구한다.
+
+캐릭터는 항상 (0,0) 목적지는 (n-1,m-1)
+
 '''
 from collections import deque
-
 def solution(maps):
     n,m = len(maps), len(maps[0])
-    visited = set()
-    q = deque()
+    #print(n,m)
+    def bfs(start):
+        visited = set()
+        queue = deque()
+        # 갈 곳을 queue에 넣는다.
+        queue.append((0,0,1))
+        visited.add((0,0))
+        while queue:
+            y,x,d = queue.popleft()
+            if (y,x) == (n-1,m-1):
+                return d
+            for dy, dx in [(0,1),(1,0),(-1,0),(0,-1)]:
+                yy = y + dy
+                xx = x + dx
+                if 0 <= yy < n and 0 <= xx < m and maps[yy][xx] == 1:
+                    if (yy,xx) not in visited:
+                        queue.append((yy,xx,d+1))
+                        visited.add((yy,xx))
+        return -1
+            
+        # queue에서 popleft한다.
+        # 해당 지점 방문 여부 판단하여 방문 안했을 시에만 다음 갈 곳 queue에 넣기
     
-    visited.add((0,0))
-    q.append((0,0,1))
+    return bfs((0,0))
     
-    while q:
-        y,x,l = q.popleft()
-        if (y,x) == (n-1,m-1):
-            return l
-        for dy, dx in [(1,0),(0,1),(-1,0),(0,-1)]:
-            yy,xx = y+dy, x+dx
-            if 0 <= yy < n and 0 <= xx < m and maps[yy][xx] == 1:
-                if not (yy,xx) in visited:
-                    visited.add((yy,xx))
-                    q.append((yy,xx,l+1))
-    return -1
-                
